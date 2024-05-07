d235d3ba820719c3c2b55a908db276f5a0adc804
davidwalterm <david.walter.marinescu@gmail.com>
2024-05-07 20:50:09 +0300

    Added Function Examples

---
+++ recursion.py
@@ -0,0 +1,10 @@
+def try_recursion(k):
+    if(k > 0):
+        result = k + try_recursion(k - 1)
+        print(result)
+    else:
+        result = 0
+    return result
+
+print("\n\nRecursion Example Results")
+try_recursion(6)
