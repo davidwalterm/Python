8dcf6c512a43ab49fa9d8ea11e7172ccd9f1e89d
davidwalterm <david.walter.marinescu@gmail.com>
2024-05-11 21:12:00 +0300

    Added Math Examples

---
+++ round_number_up_and_down_to_integer.py
@@ -0,0 +1,11 @@
+#Import math library
+import math
+
+#Round a number upward to its nearest interger
+x = math.ceil(1.4)
+
+#Round a number downward to its nearest interger
+y = math.floor(1.4)
+
+print(x)
+print(y)
